930fb8b7443659a38c602520ec0458990703fc70
TenQX <161852838+TenQX@users.noreply.github.com>
2024-03-15 16:48:09 +0300

    Add files via upload

---
+++ Project E.cpp
@@ -0,0 +1,31 @@
+﻿// Project C.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	int min = 0, max = 9;
+	const int SIZE = 10;
+	int a[SIZE];
+	for (int b = 0; b < SIZE; b++) {
+		a[b] = min + rand() % (max + 1 - min);
+		cout << a[b] << " ";
+	}
+	cout << endl;
+	bool flag;
+	for (int c = 0; c < SIZE; c++) {
+		flag = true;
+		for (int b = 0; b < SIZE - 1 - c; b++) {
+			if (a[b] > a[b + 1]) {
+				flag = false;
+				swap(a[b], a[b + 1]);
+			}
+		}
+		if (flag != false) break;
+	}
+	for (int b = 0; b < SIZE; b++) {
+		cout << a[b] << " ";
+	}
+}
