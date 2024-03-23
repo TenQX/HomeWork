40469bd9db882c98356a56b4b9698be7f18d8146
TenQX <161852838+TenQX@users.noreply.github.com>
2024-03-23 20:08:39 +0300

    Add files via upload

---
+++ Project G.cpp
@@ -0,0 +1,42 @@
+﻿// Project C.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    const int SIZE = 10;
+    int array(SIZE);
+    bool flag = true;
+    int left = 0, right = SIZE - 1;
+    while ((left < right) && flag > 0)
+    {
+        flag = 0;
+        for (int i = left; i < right; i++) 
+        {
+            if (array[i] > array[i + 1]) 
+            {
+                double t = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = t;
+                flag = 1;   
+        }
+        right--; 
+        for (int i = right; i > left; i--) 
+        {
+            if (array[i - 1] > array[i])
+            {
+                double t = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = t;
+                flag = 1;   
+            }
+        }
+        left++;
+        if (flag == 0) break;
+    }
+    for (int i = 0, i < SIZE, i++) {
+        cout << array[i] << " ";
+    }
+}
