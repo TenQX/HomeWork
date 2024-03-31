2d9f298ebe45bb098fd4aa6e5fe1bd5e195777db
TenQX <161852838+TenQX@users.noreply.github.com>
2024-03-31 20:16:47 +0300

    Add files via upload

---
+++ Project I.cpp
@@ -0,0 +1,34 @@
+﻿// Project I.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int power(int basenum, int powernum) {
+    return pow(basenum, powernum);
+}
+
+int summa(int startnum, int finishnum) {
+    int q = 0;
+    for (int m = startnum + 1; m < finishnum; m++) q += m;
+    return q;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    //int a, b;
+    //cin >> a >> b;
+
+    //int ans = power(a, b);
+    //cout << ans;
+
+    //int ans = summa(a, b);
+    //cout << ans;
+
+
+
+
+
+}
