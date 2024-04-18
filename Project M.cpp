6671908da20a3e4b5d7b152107b80eb6c0428d6f
TenQX <161852838+TenQX@users.noreply.github.com>
2024-04-18 16:40:45 +0300

    Add files via upload

---
+++ Project M.cpp
@@ -0,0 +1,16 @@
+﻿// Project M.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define q rand() % 9
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "rus");
+    int* arr = new int [5];
+    int* parr = arr;
+    cout << parr;
+}
