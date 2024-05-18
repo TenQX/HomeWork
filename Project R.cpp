e2a39940008278323481e0aecf9c9058261881a5
TenQX <161852838+TenQX@users.noreply.github.com>
2024-05-18 13:30:45 +0300

    Add files via upload

---
+++ Project R.cpp
@@ -0,0 +1,42 @@
+﻿// Project R.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define q rand() % 9
+#define p 1 + rand() % 8
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+	int maxLen;
+	cin >> maxLen;
+	string path = "File.txt";
+	string pathCopy = "File(copy).txt";
+	ofstream fout;
+	fout.open(pathCopy, ofstream::app);
+	if (!fout.is_open()) {
+		cout << "Не удалось открыть или создать файл" << pathCopy;
+	}
+	else {
+		ifstream fin;
+		fin.open(path);
+		if (!fin.is_open()) {
+			cout << "Не удалось открыть файл" << path;
+		}
+		else {
+			string str;
+			while (!fin.eof()) {
+				str = "";
+				getline(fin, str);
+				if (str.length() <= maxLen) {
+					fout << str << endl;
+				}
+			}
+		}
+	}
+}
